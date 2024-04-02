0737b4f151c91cd81a71cf5ce9432cf6508a8b4d
Nashla07 <124918444+Nashla07@users.noreply.github.com>
2024-04-02 13:25:06 -0600

    Add files via upload

---
+++ nashla_sketch.js
@@ -0,0 +1,8 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//Pixelscape
+//Marianne Teixido
+//https://github.com/MarianneTeixido
+
+osc(21,0.1,0.9).kaleid(12).shift(1,5,0.5).out(o0)
+
+
